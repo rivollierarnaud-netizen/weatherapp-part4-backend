53cc4e0e0840f02e94b10a7ab664e68543b52dd1
rivollierarnaud-netizen <rivollierarnaud@gmail.com>
2025-10-31 22:46:16 +0100

    back2

--- models/connection.js
+++ models/connection.js
@@ -1,6 +1,6 @@
 const mongoose = require('mongoose');
 
-const connectionString = 'mongodb+srv://rivollier:admin@cluster0.lsoov5a.mongodb.net/weatherapp4';
+const connectionString = process.env.CONNECTION_STRING;
 
 mongoose.connect(connectionString, { connectTimeoutMS: 2000 })
   .then(() => console.log('Database connected'))
