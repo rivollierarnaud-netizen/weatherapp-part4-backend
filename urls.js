ca0fb00c28035c4ade45542f4700b6eef6e4bd59
rivollierarnaud-netizen <rivollierarnaud@gmail.com>
2025-10-31 22:33:42 +0100

    weatherapp-part4-2

--- urls.js
+++ urls.js
@@ -1,7 +1,7 @@
 // Insert the backend URL given by Vercel in this variable
-const BACKEND_URL = '';
+const BACKEND_URL = 'https://weatherapp-part4-backend-inky.vercel.app';
 
 // Insert the frontend URL given by Vercel in this variable
-const FRONTEND_URL = '';
+const FRONTEND_URL = 'https://weatherapp-part4-frontend-navy.vercel.app';
 
 module.exports = { BACKEND_URL, FRONTEND_URL }; // Do not edit/remove this line
